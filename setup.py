cba87b17e571150b4f37dc4abbad1f7486aba07d
ErikKalkoken <erik.kalkoken@gmail.com>
2023-04-18 16:09:49 +0200

    Use dynamic version in setup

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 import os
 from setuptools import find_packages, setup
-
+from dhooks_lite.constants import APP_VERSION
 
 # read the contents of your README file
 this_directory = os.path.abspath(os.path.dirname(__file__))
@@ -12,7 +12,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name="dhooks_lite",
-    version="0.7.0",
+    version=APP_VERSION,
     packages=find_packages(),
     include_package_data=True,
     license="MIT",
